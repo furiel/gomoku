8199e5f559bd218693abad12df71cb3b83c30e75
Antal Nemes <antal.nemes.hu@gmail.com>
2020-04-10 20:19:59 +0200

    http-kit instead of immutant
    Signed-off-by: Antal Nemes <antal.nemes.hu@gmail.com>

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.597"]
-                 [org.immutant/immutant "2.1.10"]
+                 [ring/ring-core "1.8.0"]
+                 [http-kit "2.3.0"]
                  [com.bhauman/figwheel-main "0.2.3"]
                  [org.clojure/core.async "1.1.587"]
                  [com.bhauman/rebel-readline-cljs "0.1.4"]
                  [reagent "0.10.0"]]
-  :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]}
+  :aliases {"fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]}
   :main ^:skip-aot gomoku.core
   :target-path "target/%s"
   :resource-paths ["target" "resources"]
--- src/gomoku/core.clj
+++ src/gomoku/core.clj
@@ -1,14 +1,20 @@
 (ns gomoku.core
-  (:require [immutant.web :refer :all]
-            [immutant.web.async :as async])
+  (:require
+   [org.httpkit.server :refer [run-server send! with-channel on-close on-receive]]
+   [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+   [ring.util.response :refer [resource-response content-type]])
   (:gen-class))
 
-(def callbacks
-  {:on-message (fn [ch msg]
-                 (async/send! ch (.toUpperCase msg)))})
-
-(defn app [request]
-  (async/as-channel request callbacks))
+(defn handler [req]
+  (or
+   (when (= "/" (:uri req))
+     (some-> (resource-response "index.html" {:root "public"})
+             (content-type "text/html; charset=utf-8")))
+   {:status 404
+    :headers {"Content-Type" "text/html"}
+    :body "Not found"}))
 
 (defn -main []
-  (run app))
+  (run-server
+   (wrap-defaults handler site-defaults)
+   {:port 3000}))
