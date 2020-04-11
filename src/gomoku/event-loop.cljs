57e310c47112f2d50ace5f35578820cc7e332e1d
Antal Nemes <antal.nemes.hu@gmail.com>
2020-04-11 10:19:23 +0200

    pass action through server
    Signed-off-by: Antal Nemes <antal.nemes.hu@gmail.com>

--- src/gomoku/event-loop.cljs
+++ src/gomoku/event-loop.cljs
@@ -1,6 +1,7 @@
 (ns gomoku.event-loop
   (:require
    [cljs.core.async :refer [chan put! <! go go-loop]]
+   [gomoku.websockets :refer [send-msg!]]
    [gomoku.board :refer [draw-me]]))
 
 (def event-queue (chan))
@@ -12,8 +13,9 @@
     (recur (<! event-queue))))
 
 (defn click-event [x y]
-  (put! event-queue
-        {:click {:player :x
-                 :point [x y]}}))
+  (send-msg!
+   {:click {:player :x
+            :point [x y]}}))
 
-(defn handle-message [msg])
+(defn handle-message [msg]
+  (put! event-queue msg))
--- src/gomoku/websockets.clj
+++ src/gomoku/websockets.clj
@@ -1,7 +1,10 @@
 (ns gomoku.websockets
   (:require
+   [cognitect.transit :as transit]
    [org.httpkit.server :refer [send! with-channel on-close on-receive]])
   (:gen-class))
+
+(import [java.io ByteArrayInputStream ByteArrayOutputStream])
 
 (defonce channels (atom #{}))
 
@@ -11,15 +14,28 @@
 (defn disconnect! [channel status]
  (swap! channels #(remove #{channel} %)))
 
+(defn read-msg [transit-msg]
+  (let [in (ByteArrayInputStream. (.getBytes transit-msg))
+        reader (transit/reader in :json)
+        msg (transit/read reader)]
+    msg))
+
+(defn write-msg [msg]
+  (let [out (ByteArrayOutputStream. 4096)
+        writer (transit/writer out :json)]
+    (transit/write writer msg)
+    (let [transit-msg (.toString out)]
+      transit-msg)))
+
 (defn notify-clients [msg]
- (doseq [channel @channels]
-     (send! channel msg)))
+  (doseq [channel @channels]
+    (send! channel (write-msg msg))))
 
 (defn ws-handler [request]
   (with-channel request channel
     (connect! channel)
     (on-close channel (partial disconnect! channel))
-    (on-receive channel #(notify-clients %))))
+    (on-receive channel #(notify-clients (read-msg %)))))
 
 (defn wrap-ws [handler]
   (fn [req]
