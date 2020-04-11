0da3931d2c06460f938b99a553fd05aad1ceaa6b
Antal Nemes <antal.nemes.hu@gmail.com>
2020-04-11 08:52:44 +0200

    factoring out event-loop into separate module
    Signed-off-by: Antal Nemes <antal.nemes.hu@gmail.com>

--- src/gomoku/core.cljs
+++ src/gomoku/core.cljs
@@ -2,28 +2,17 @@
   (:require
    [reagent.dom :as rd]
    [reagent.core :as r]
-   [cljs.core.async :refer [chan put! <! go go-loop timeout]]
+   [gomoku.event-loop :refer [start-event-loop click-event]]
    [gomoku.websockets :refer [make-websocket!]]
-   [gomoku.board :as board]))
+   [gomoku.board :refer [board]]))
 
-(def event-queue (chan))
 (def websocket-url (str "ws://" (.-host js/location) "/ws"))
 
-(defn start-event-loop []
-  (go-loop [event (<! event-queue)]
-    (let [{{[x y] :point} :click} event]
-      (board/draw-me x y))
-    (recur (<! event-queue))))
-
-(defn click-event [x y]
-  (put! event-queue
-        {:click {:player :x
-                 :point [x y]}}))
 (defn page []
   [:div
    [:div "Board"]
-   [:div (board/board {:dimension [10 10]
-                       :on-click click-event})]])
+   [:div (board {:dimension [10 10]
+                 :on-click click-event})]])
 
 (defn get-app-element []
   (.getElementById js/document "app"))
---
+++ src/gomoku/event-loop.cljs
@@ -0,0 +1,17 @@
+(ns gomoku.event-loop
+  (:require
+   [cljs.core.async :refer [chan put! <! go go-loop]]
+   [gomoku.board :refer [draw-me]]))
+
+(def event-queue (chan))
+
+(defn start-event-loop []
+  (go-loop [event (<! event-queue)]
+    (let [{{[x y] :point} :click} event]
+      (draw-me x y))
+    (recur (<! event-queue))))
+
+(defn click-event [x y]
+  (put! event-queue
+        {:click {:player :x
+                 :point [x y]}}))
