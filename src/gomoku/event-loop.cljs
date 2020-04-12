679443d2d9771f1df67d6b4de45913da964df386
Antal Nemes <antal.nemes.hu@gmail.com>
2020-04-12 17:05:09 +0200

    preparing for better dispatch
    Signed-off-by: Antal Nemes <antal.nemes.hu@gmail.com>

--- src/gomoku/event-loop.cljs
+++ src/gomoku/event-loop.cljs
@@ -6,11 +6,15 @@
 
 (def event-queue (chan))
 
+(defn handle-click-event [event]
+  (let [{{[x y] :point who :player} :click} event]
+    (draw-me x y who)))
+
 (defn start-event-loop []
   (go-loop [event (<! event-queue)]
-    (let [{{[x y] :point who :player} :click} event]
-      (draw-me x y who))
-    (recur (<! event-queue))))
+    (cond
+      (contains? event :click) (handle-click-event event))
+      (recur (<! event-queue))))
 
 (defn click-event [x y who]
   (send-msg!
