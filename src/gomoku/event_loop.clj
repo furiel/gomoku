b1ef21af8aaed221bf20b5726f6558d2ca1788dd
Antal Nemes <antal.nemes.hu@gmail.com>
2020-04-19 15:03:35 +0200

    testing a complete game
    Signed-off-by: Antal Nemes <antal.nemes.hu@gmail.com>

--- src/gomoku/event_loop.clj
+++ src/gomoku/event_loop.clj
@@ -9,18 +9,8 @@
 (defn stop-event-loop [event-loop]
     (close! event-loop))
 
-(defn sync-event-loop [event-loop]
-  (let [p (promise)]
-    (put-event! event-loop {:event 'sync :data p})
-    (deref p 1000 'timed-out)))
-
-(defn sync-handler [state event]
-  (deliver (:data event) 'done)
-  event)
-
 (defn start-event-loop [initial-state event-handlers]
-  (let [event-loop (chan)
-        event-handlers (merge event-handlers {'sync sync-handler})]
+  (let [event-loop (chan)]
     (go-loop [event (<! event-loop) state initial-state]
       (when event
         (when (not (contains? event :event))
--- test/gomoku/websockets_test.clj
+++ test/gomoku/websockets_test.clj
@@ -1,16 +1,42 @@
 (ns gomoku.websockets-test
   (:require [clojure.test :refer :all]
+            [clojure.core.async :refer [chan put! alts!! close! timeout]]
             [gomoku.websockets :refer :all]
             [gomoku.board :refer :all]))
 
+(defn pop-message! [msg-queue]
+  (let [[msg t] (alts!! [msg-queue (timeout 1000)])]
+    (is (not (nil? msg)))
+    msg))
+
 (deftest test-websockets
   (testing "test-websockets"
-    (let [msg-queue-1 (atom nil)
-          msg-queue-2 (atom nil)
-          notify! (fn [channel msg] (reset! (if (= channel 1)
-                                                  msg-queue-1 msg-queue-2) msg))
+    (let [q1 (chan 10)
+          q2 (chan 10)
+          notify! (fn [channel msg] (put! (if (= channel 1) q1 q2) msg))
           game (start-new-game {:notify notify!})]
       (connect! game 1)
-      (is (= 'done (sync-game game)))
-      (is (= (:message @msg-queue-1) "Successfully joined! Waiting for other players ..."))
+      (is (= (:message (pop-message! q1)) "Successfully joined! Waiting for other players ..."))
+      (connect! game 2)
+      (is (= (:message (pop-message! q2)) "Successfully joined! Waiting for other players ..."))
+
+      (is (= (:event (pop-message! q1)) 'display))
+      (is (= (:event (pop-message! q2)) 'display))
+
+      (dotimes [n 4]
+        (read-event game 1 {:event 'click :point [0 n]})
+        (is (= (:event (pop-message! q1)) 'click))
+        (is (= (:event (pop-message! q2)) 'click))
+
+        (read-event game 2 {:event 'click :point [1 n]})
+        (is (= (:event (pop-message! q1)) 'click))
+        (is (= (:event (pop-message! q2)) 'click)))
+
+      (read-event game 1 {:event 'click :point [0 4]})
+      (is (= (:event (pop-message! q1)) 'click))
+      (is (= (:event (pop-message! q2)) 'click))
+
+      (is (= (:message (pop-message! q1)) "GG"))
+      (is (= (:message (pop-message! q2)) "GG"))
+
       (stop-game game))))
